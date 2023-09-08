0a8d1fc11dd65d8fb33ba0a6ca0f8411aeb35515
rahdi <adrian.heiden@gmail.com>
2023-09-08 14:16:43 +0200

    added tests for 'login' and 'labeled input' components

--- src/app/modules/auth/pages/login/login.component.spec.ts
+++ src/app/modules/auth/pages/login/login.component.spec.ts
@@ -1,24 +1,99 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { SharedModule } from 'shared';
+import { Path, SharedModule } from 'shared';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthModule } from 'modules/auth/auth.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [RouterTestingModule, SharedModule],
+      imports: [RouterTestingModule, SharedModule, AuthModule],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display .card', () => {
+    const card = fixture.nativeElement.querySelector('.card');
+    expect(card).toBeTruthy();
+  });
+
+  it('should display title', () => {
+    const title = fixture.nativeElement.querySelector('h4');
+    expect(title).toBeTruthy();
+    expect(title.innerText).toBe('Log in');
+  });
+
+  it('should display "username" labeled input', () => {
+    const input = fixture.nativeElement.querySelector('#login-username');
+    const label = input.previousSibling;
+    expect(input).toBeTruthy();
+    expect(label).toBeTruthy();
+    expect(label.innerText).toBe('Username');
+  });
+
+  it('should display "password" labeled input', () => {
+    const input = fixture.nativeElement.querySelector('#login-password');
+    const label = input.previousSibling;
+    expect(input).toBeTruthy();
+    expect(label).toBeTruthy();
+    expect(label.innerText).toBe('Password');
+  });
+
+  it('should display a text offering registration', fakeAsync(() => {
+    const text = fixture.nativeElement.querySelector('p');
+    expect(text).toBeTruthy();
+    expect(text.innerText).toBe("Don't have an account? Create one here!");
+
+    const link = text.children[0];
+    expect(link).toBeTruthy();
+    expect(link.innerText).toBe('here');
+    link.click();
+    tick();
+
+    expect(location.path()).toBe(`/${Path.Register}`);
+  }));
+
+  it('should display a link to going back', fakeAsync(() => {
+    const link = fixture.nativeElement.querySelector('.btn-secondary');
+    expect(link).toBeTruthy();
+    expect(link.innerText).toBe('Go back');
+
+    link.click();
+    tick();
+
+    expect(location.path()).toBe(`/${Path.ChooseOption}`);
+  }));
+
+  it('should display a link to go to dashboard', fakeAsync(() => {
+    const link = fixture.nativeElement.querySelector('.btn-primary');
+    expect(link).toBeTruthy();
+    expect(link.innerText).toBe('Log in');
+
+    link.click();
+    tick();
+
+    expect(location.path()).toBe(`/${Path.Empty}`);
+  }));
 });
--- src/app/shared/components/labeled-input/labeled-input.component.spec.ts
+++ src/app/shared/components/labeled-input/labeled-input.component.spec.ts
@@ -8,14 +8,38 @@ describe('LabeledInputComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LabeledInputComponent]
+      declarations: [LabeledInputComponent],
     });
     fixture = TestBed.createComponent(LabeledInputComponent);
     component = fixture.componentInstance;
+    component.id = 'my-test-id';
+    component.label = 'TestLabel';
+    component.type = 'password';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a properly created label', () => {
+    const label = fixture.nativeElement.querySelector('label');
+    expect(label).toBeTruthy();
+    expect(label.innerText).toBe(component.label);
+
+    const attrFor = label.getAttribute('for');
+    expect(attrFor).toBe(component.id);
+  });
+
+  it('should have a properly created input', () => {
+    const input = fixture.nativeElement.querySelector('input');
+    expect(input).toBeTruthy();
+
+    const attrId = input.getAttribute('id');
+    const attrName = input.getAttribute('name');
+    const attrType = input.getAttribute('type');
+    expect(attrId).toBe(component.id);
+    expect(attrName).toBe(component.id);
+    expect(attrType).toBe(component.type);
   });
 });
