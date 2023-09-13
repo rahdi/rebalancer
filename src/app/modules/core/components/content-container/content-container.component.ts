994093e89b5527523c864245cede1678a87a5272
rahdi <adrian.heiden@gmail.com>
2023-09-13 12:32:13 +0200

    added transition to dialog's backdrop

--- src/app/modules/core/components/content-container/content-container.component.ts
+++ src/app/modules/core/components/content-container/content-container.component.ts
@@ -4,7 +4,21 @@ import { Path } from 'shared';
 @Component({
   selector: 'app-content-container',
   templateUrl: './content-container.component.html',
-  styleUrls: ['./content-container.component.css'],
+  styles: [
+    `
+      dialog {
+        margin-right: -320px;
+        box-shadow: 0 0 0 120vmax rgba(0, 0, 0, 0);
+      }
+      dialog.isOpen {
+        margin-right: 0;
+        box-shadow: 0 0 0 120vmax rgba(0, 0, 0, 0.5);
+      }
+      dialog::backdrop {
+        background-color: unset;
+      }
+    `,
+  ],
 })
 export class ContentContainerComponent {
   @Input() header = '';
